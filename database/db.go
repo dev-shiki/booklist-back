f222c2abd69db10979ee427d19b30976aaafbd41
shiki <nandaleoardana@gmail.com>
2024-10-02 01:05:55 +0700

    Update db.go

--- database/db.go
+++ database/db.go
@@ -11,8 +11,15 @@ var DB *sql.DB
 
 func Connect() {
 	var err error
-	DB, err = sql.Open("mysql", "shiki:1234@tcp(localhost:3306)/booklist")
+	DB, err = sql.Open("mysql", "sql12734740:45bu1mdIKc@tcp(52.76.27.242:3306)/sql12734740?allowCleartextPasswords=true")
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	} else {
+		log.Println("Successfully connected to the database!")
 	}
 }
